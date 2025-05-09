5098c0b24f0917b727071d9c886de1afe3fbda82
MarsGotBars <marcinm@live.nl>
2025-05-09 19:49:33 +0200

    Update server.js
    created functions for project retrieval, still needs some work regarding the err page

--- server.js
+++ server.js
@@ -15,8 +15,31 @@ app.set("views", "./views");
 
 const dailyNotes = await readdir("content/daily_notes");
 
-const readProjectJSON = await readFile("content/JSON/projects.json", "utf-8");
-const allProjects = JSON.parse(readProjectJSON);
+let allProjects;
+const retrieveProjects = async () => {
+  console.log("gathering...");
+  
+  const readProjectJSON = await readFile("content/JSON/projects.json", "utf-8");
+  allProjects = JSON.parse(readProjectJSON);
+}
+
+const findProject = (providedSlug) => {  
+  try {
+    const project = allProjects.find((project) => project.slug === providedSlug);
+    // throwing an error does not work..?
+    if(project === undefined){
+      throw new Error("No project found")
+    }
+    
+    return {project}
+  } catch (error) {
+    const errPage = "err"
+    console.log(errPage);
+    return {errPage}
+  }
+}
+
+retrieveProjects();
 
 // routes
 app.get("/", async function (request, response) {
@@ -26,12 +49,32 @@ app.get("/", async function (request, response) {
   });
 });
 
-app.get("/portfolio", async function (request, response) {
+app.get("/err", async function (request, response) {
+  response.render("err.liquid", {
+    styles: ["home.css"],
+    scripts: ["moshing.js", "mouse-follow.js"]
+  });
+});
 
+app.get("/portfolio", async function (request, response) {
+    console.log(allProjects);
+    
     response.render("portfolio.liquid", {
       styles: ["portfolio.css", "accessible-link.css"],
-      allProjects
+      allProjects,
     })
+})
+
+app.get("/portfolio/:slug", async function (request, response) {
+  const {slug} = request.params;
+  const {project, errPage} = findProject(slug)
+
+  console.log(project, "???", errPage);
+  if(errPage){
+    response.redirect(303, `/${errPage}`)
+  }
+  
+  response.render("project.liquid", { project });
 })
 
 app.get("/journal", async function (request, response) {
