b27131db994452eafd8d004cb9a01eed2c356a87
MarsGotBars <marcinm@live.nl>
2025-09-29 22:30:56 +0200

    moved data to root

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,7 @@ export default function (eleventyConfig) {
       input: "views",
       includes: "partials",
       layouts: "layouts",
+      data: "../_data",      // <-- Go up one level from 'views' to reach root/_data
       output: "dist",
     },
     templateFormats: ["liquid", "md", "html"],
