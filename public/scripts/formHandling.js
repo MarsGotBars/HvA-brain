8219654734d1bb16463249e39399001fb5bf9b60
MarsGotBars <marcinm@live.nl>
2025-11-13 09:47:54 +0100

    changes

--- public/scripts/formHandling.js
+++ public/scripts/formHandling.js
@@ -1,2 +1,14 @@
-const formInputs = document.querySelectorAll('[data-form="input"]')
-const formSubmit = document.querySelector('[data-form="submit"]')
+const form = document.querySelector('[data-form="form"');
+const formInputs = form?.querySelectorAll('[data-form="input"]');
+const formSubmit = form?.querySelector('[data-form="submit"]');
+
+const formInputsData = []
+formInputs.forEach((input)=>{
+    formInputsData.push(input.value)
+})
+console.log(formInputs);
+
+
+form.addEventListener("submit", (e)=>{
+    console.log(e);
+})
