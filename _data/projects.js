50e9b4a8d16f36df550d55c72a9ad53ebb067b73
MarsGotBars <marcinm@live.nl>
2025-11-21 13:25:31 +0100

    Update projects.js

--- _data/projects.js
+++ _data/projects.js
@@ -9,11 +9,17 @@ const projectsJSON = fs.readFileSync(
   path.join(__dirname, "../content/JSON/projects.json"),
   "utf-8"
 );
+
+// Rewrite this to filter OUT the broken projects rather than doing this in the template
 const allProjects = JSON.parse(projectsJSON);
+const brokenProject = allProjects.filter((e)=> e.progress == "BROKEN")
+console.log(brokenProject);
+const length = allProjects.length - brokenProject.length
 
 console.log("Loaded projects data with", allProjects.length, "projects.", allProjects);
 
 // Export the data directly, not wrapped in a function
 export default {
   allProjects,
+  length
 };
