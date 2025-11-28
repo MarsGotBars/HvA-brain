285723a6f3103d00ae55b794bd4009d876330837
MarsGotBars <marcinm@live.nl>
2025-11-28 21:04:20 +0100

    form handling

--- public/scripts/formHandling.js
+++ public/scripts/formHandling.js
@@ -1,14 +1,38 @@
-const form = document.querySelector('[data-form="form"');
-const formInputs = form?.querySelectorAll('[data-form="input"]');
-const formSubmit = form?.querySelector('[data-form="submit"]');
+// const form = document.querySelector('[data-form="form"');
+// const formInputs = form?.querySelectorAll('[data-form="input"]');
+// const formSubmit = form?.querySelector('[data-form="submit"]');
 
-const formInputsData = []
-formInputs.forEach((input)=>{
-    formInputsData.push(input.value)
-})
-console.log(formInputs);
+// const formInputsData = [];
+// formInputs.forEach((input) => {
+//   formInputsData.push(input.value);
+// });
+// console.log(formInputs);
 
+// form.addEventListener("submit", (e) => {
+//   console.log(e);
+// });
+console.log("Initialized selector");
 
-form.addEventListener("submit", (e)=>{
-    console.log(e);
-})
+class FormHandler {
+  constructor(selector) {
+    this.form = document.querySelector(selector);
+    
+    this.inputs = this.form.querySelectorAll(`${selector} [data-form="input"]`);
+    this.submit = this.form.querySelector(`${selector} [data-form="submit"]`);
+    
+    this.registerEvents();
+  }
+
+  registerEvents() {
+    this.form.addEventListener("submit", this.onSubmit.bind(this));
+  }
+
+  onSubmit(e) {
+    console.log(e)
+    // e.preventDefault();
+
+    console.log("Form submitted");
+  }
+}
+
+const form = new FormHandler('[data-form="filtering"]');
